0bf0f4bfef726d3980346158c8c27141794acd6b
MMsira03 <siracha5781@gmail.com>
2022-02-10 16:32:50 +0700

    แก้ไข

--- src/routes.js
+++ src/routes.js
@@ -15,6 +15,8 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
+
+//
 import Dashboard from "views/Dashboard.js";
 import Notifications from "views/Notifications.js";
 import Icons from "views/Icons.js";
