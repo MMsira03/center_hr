0dc102b74b10dd0f25fb539d720f7365a1f50cac
MMsira03 <siracha5781@gmail.com>
2022-02-15 09:33:08 +0700

    ้ฟฟฟ

--- src/routes.js
+++ src/routes.js
@@ -15,10 +15,6 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-
-//
-//
-
 import Dashboard from "views/Dashboard.js";
 import Notifications from "views/Notifications.js";
 import Icons from "views/Icons.js";
